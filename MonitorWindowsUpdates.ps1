fdd590e2d671b540c360e5d76a127a894639ecda
Loïck STUMPERT <stumpert.loick@gmail.com>
2022-08-19 13:56:26 +0200

    Delete Write-host

--- MonitorWindowsUpdates.ps1
+++ MonitorWindowsUpdates.ps1
@@ -5,10 +5,6 @@ $Criticals = $Updates | where {$_.MsrcSeverity -eq "Critical"}
 $Importants = $Updates | where {$_.MsrcSeverity -eq "Imporant"}
 $Others = $Updates | where {$_.MsrcSeverity -eq $null}
 
-Write-Host "critical=$($Criticals.count)"
-Write-Host "important=$($Importants.count)"
-Write-Host "other=$($Others.count)"
-
 &'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k critical.updates -o $Criticals.count
 &'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k important.updates -o $Importants.count
 &'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k others.updates -o $Others.count
