c0ed561fc231d9f7b9f2023d5c7b42290b9c7731
Aloïs Micard <a.micard@vold.lu>
2022-08-19 10:48:18 +0200

    Cleanup script

--- MonitorWindowsUpdates.ps1
+++ MonitorWindowsUpdates.ps1
@@ -1,22 +1,10 @@
-#Get all updates that are not install
 $Searcher = new-object -com "Microsoft.Update.Searcher"
-$Searcher.Search("IsAssigned=1 and IsInstalled=0").Updates | Format-Table title, MsrcSeverity, IsInstalled
+$Updates = $Searcher.Search("IsAssigned=1 and IsInstalled=0").Updates
 
-#Type of Windows updates by severity
-$Critical = $Searcher.updates |
+$Criticals = $Updates | where {$_.MsrcSeverity -eq "Critical"}
+$Importants = $Updates | where {$_.MsrcSeverity -eq "Imporant"}
+$Others = $Updates | where {$_.MsrcSeverity -eq $null}
 
-      where {$_.MsrcSeverity -eq "Critical"}
-
-$Important = $Searcher.updates |
-
-      where {$_.MsrcSeverity -eq "Imporant"}
-
-$other = $Searcher.updates |
- 
-      where {$_.MsrcSeverity -eq $null}
-
-#Write results 
-Write-Host "total=$($Searcher.updates.count)"
-Write-Host "critical=$($Critical.count)"
-Write-Host "important=$($Important.count)"
-Write-Host "other=$($other.count)"
+Write-Host "critical=$($Criticals.count)"
+Write-Host "important=$($Importants.count)"
+Write-Host "other=$($Others.count)"
