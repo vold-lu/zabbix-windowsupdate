bb4502f69014149d3182a759bcf16935a3b01f72
Loïck STUMPERT <stumpert.loick@gmail.com>
2022-08-19 13:51:52 +0200

    Add Zabbix-sender in the script

--- MonitorWindowsUpdates.ps1
+++ MonitorWindowsUpdates.ps1
@@ -1,5 +1,5 @@
 $Searcher = new-object -com "Microsoft.Update.Searcher"
-$Updates = $Searcher.Search("IsAssigned=1 and IsInstalled=0").Updates
+$Updates = $Searcher.Search("IsAssigned=1 and IsInstalled=0").Updates.Count
 
 $Criticals = $Updates | where {$_.MsrcSeverity -eq "Critical"}
 $Importants = $Updates | where {$_.MsrcSeverity -eq "Imporant"}
@@ -8,3 +8,7 @@ $Others = $Updates | where {$_.MsrcSeverity -eq $null}
 Write-Host "critical=$($Criticals.count)"
 Write-Host "important=$($Importants.count)"
 Write-Host "other=$($Others.count)"
+
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k critical.updates -o $Criticals.count
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k important.updates -o $Importants.count
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k others.updates -o $Others.count
