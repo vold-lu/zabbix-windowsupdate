71ccf49d9be6946fb8e6695d0f5767d863183098
Aloïs Micard <a.micard@vold.lu>
2022-08-19 14:42:49 +0200

    Update script

--- MonitorWindowsUpdates.ps1
+++ MonitorWindowsUpdates.ps1
@@ -1,10 +1,10 @@
 $Searcher = new-object -com "Microsoft.Update.Searcher"
-$Updates = $Searcher.Search("IsAssigned=1 and IsInstalled=0").Updates.Count
+$Updates = $Searcher.Search("IsAssigned=1 and IsInstalled=0").Updates
 
 $Criticals = $Updates | where {$_.MsrcSeverity -eq "Critical"}
 $Importants = $Updates | where {$_.MsrcSeverity -eq "Imporant"}
 $Others = $Updates | where {$_.MsrcSeverity -eq $null}
 
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k critical.updates -o $Criticals.count
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k important.updates -o $Importants.count
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k others.updates -o $Others.count
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k windows.critical.updates -o $Criticals.count
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k windows.important.updates -o $Importants.count
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k windows.others.updates -o $Others.count
