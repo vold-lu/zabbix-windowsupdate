fce2d0aa2931af6eed23211e1adac8d422678cd1
Aloïs Micard <a.micard@vold.lu>
2022-08-19 14:48:39 +0200

    Use config file

--- MonitorWindowsUpdates.ps1
+++ MonitorWindowsUpdates.ps1
@@ -5,6 +5,6 @@ $Criticals = $Updates | where {$_.MsrcSeverity -eq "Critical"}
 $Importants = $Updates | where {$_.MsrcSeverity -eq "Imporant"}
 $Others = $Updates | where {$_.MsrcSeverity -eq $null}
 
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k windows.critical.updates -o $Criticals.count
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k windows.important.updates -o $Importants.count
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -z 192.168.1.25 -s "srv-test" -k windows.others.updates -o $Others.count
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.critical.updates -o $Criticals.count
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.important.updates -o $Importants.count
+&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.others.updates -o $Others.count
