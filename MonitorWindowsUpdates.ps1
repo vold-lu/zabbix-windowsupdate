f214fa1eaf48b23e8cdb404a58e87345573c71b9
Aloïs Micard <a.micard@vold.lu>
2022-08-20 11:39:48 +0200

    Finalize script

--- MonitorWindowsUpdates.ps1
+++ MonitorWindowsUpdates.ps1
@@ -2,9 +2,9 @@ $Searcher = new-object -com "Microsoft.Update.Searcher"
 $Updates = $Searcher.Search("IsAssigned=1 and IsInstalled=0").Updates
 
 $Criticals = $Updates | where {$_.MsrcSeverity -eq "Critical"}
-$Importants = $Updates | where {$_.MsrcSeverity -eq "Imporant"}
+$Importants = $Updates | where {$_.MsrcSeverity -eq "Important"}
 $Others = $Updates | where {$_.MsrcSeverity -eq $null}
 
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.critical.updates -o $Criticals.count
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.important.updates -o $Importants.count
-&'C:\Program Files\Zabbix Agent\zabbix_sender.exe' -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.others.updates -o $Others.count
+zabbix_sender.exe -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.critical.updates -o $Criticals.count
+zabbix_sender.exe -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.important.updates -o $Importants.count
+zabbix_sender.exe -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.others.updates -o $Others.count
