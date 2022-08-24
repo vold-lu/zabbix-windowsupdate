5e47a83c43deb023dcdedccaaf6325ff043de044
Loïck STUMPERT <stumpert.loick@gmail.com>
2022-08-24 14:01:46 +0200

    Finalise script

--- MonitorWindowsUpdates.ps1
+++ MonitorWindowsUpdates.ps1
@@ -1,9 +1,9 @@
 $Searcher = new-object -com "Microsoft.Update.Searcher"
 $Updates = $Searcher.Search("IsAssigned=1 and IsInstalled=0").Updates
 
-$Criticals = $Updates | where {$_.MsrcSeverity -eq "Critical"}
-$Importants = $Updates | where {$_.MsrcSeverity -eq "Important"}
-$Others = $Updates | where {$_.MsrcSeverity -eq $null}
+$Criticals = $Updates | where {$_.MsrcSeverity -eq "Critical"} | measure
+$Importants = $Updates | where {$_.MsrcSeverity -eq "Important"} | measure
+$Others = $Updates | where {$_.MsrcSeverity -eq $null} | measure
 
 zabbix_sender.exe -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.critical.updates -o $Criticals.count
 zabbix_sender.exe -c "C:\Program Files\Zabbix Agent\zabbix_agentd.conf" -k windows.important.updates -o $Importants.count
